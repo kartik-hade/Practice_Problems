451ef3c51ec2d21fa4269d2938b233dbc5276268
kartik-hade <hadekartik@gmail.com>
2025-08-16 15:18:34 +0530

    first commit

--- Python/Standard_deviation.py
+++ Python/Standard_deviation.py
@@ -1,1 +1,15 @@
-
+def std_dev(X):
+    """
+    Compute the standard deviation
+    
+    Argument:
+        X: list of integers
+    Return:
+        sigma: float
+    """
+    return (sum((X[i] - (sum(X)/len(X)))**2 for i in range(len(X)))/(len(X)-1))**0.5
+    
+    
+X = [float(x) for x in input().split(',')]
+sigma = std_dev(X)
+print(f'{sigma:.2f}')
